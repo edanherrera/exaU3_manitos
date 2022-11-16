59f1146594adc5623fc8e3bac5e55703e1b9caa0
edanherrera <edanherreraga@ittepic.edu.mx>
2022-11-15 17:04:05 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'examenU3-Manitos',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
