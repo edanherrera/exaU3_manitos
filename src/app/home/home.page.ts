6539e55a99fd4f678b79db16a51e5167c6071732
edanherrera <edanherreraga@ittepic.edu.mx>
2022-11-21 22:16:09 -0700

    Add date-Token

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Reservation } from '../models/reservation';
 import { ReservationService } from '../services/reservation.service';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,8 +11,9 @@ import { ReservationService } from '../services/reservation.service';
 export class HomePage {
   reservations:Reservation[];
   reservation:Reservation;
-  
-  constructor(private reservationService:ReservationService,private router: Router) {
+  dateToday = new Date();
+  message = '';
+  constructor(private reservationService:ReservationService,private router: Router, private alertController: AlertController) {
     this.reservations = reservationService.getReservation();
     this.reservation={
       'name' : '',
@@ -25,16 +27,30 @@ export class HomePage {
   public ingresar(){
     var result = this.reservations.find(({Token})=> Token === this.reservation.Token)
     if(!(result==null)){
+      
+      
       if(result.Token?.includes('Hab')){
-        console.log('Ingresado con éxito Huesped '+result.Token);
-        this.reservationService.setCurrentUser(result.Token)
+        if(result.fIn.getDate==this.dateToday.getDate){
+          console.log('Fecha iguanita');
+          console.log('Ingresado con éxito Huesped '+result.Token);
+          this.reservationService.setCurrentUser(result.Token)
+          this.message='Ingreso con éxito';
           return this.goHomeH();
         }else{
-          console.log('Ingresado con éxito Admin '+result.Token);
-          return this.goHome();
+          this.message='Aún no está en fecha para su ingreso'
         }
+      }else{
+        if(result.fIn.getDate==this.dateToday.getDate){
+          console.log('Fecha iguanita');
+          console.log('Ingresado con éxito Admin '+result.Token);
+          this.message='Ingreso con éxito';
+          return this.goHome();
+          
+        }else{this.message='Aún no está en fecha para su ingreso';}
+      }
     }else{
       console.log("Usuario  o Contraseña incorrectos ");
+      this.message='Token incorrecto';
     }
   }
   public goHome(){
@@ -44,5 +60,15 @@ export class HomePage {
   public goHomeH(){
     
     this.router.navigate(['/client']);
+  }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Importante',
+      message: this.message,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 }
